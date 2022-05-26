0b4150bb28e4fccfc6748e0e4656a6bc5330f430
nullcracker <106293179+nullcracker@users.noreply.github.com>
2022-05-26 13:11:25 +0800

    Add files via upload

---
+++ website-baralgroup/assets/js/modal.js
@@ -0,0 +1,51 @@
+function vegetableFarm()
+{
+  Swal.fire({
+    title: 'Vegetable Farm',
+    text: 'January 2019 - Bohol Gov. Edgar Chatto is shown opening the water sprinklers in the newly-planted part of the Eskaya community-based vegetable farm in Cantaub, Sierra Bullones. Wide parts of the farm have many kinds of vegetables already fully grown.',
+    imageUrl: 'assets/img/vegetablefarmLatest.jpg',
+    imageWidth: 350,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+}
+  
+function paradiseResort()
+{
+  Swal.fire({
+    title: 'Paradise Resort',
+    text: 'Paradise Hills Resort is one of the most visited places in Bohol. It is located in the secluded place of Sierra Bullones, in Barangay Matin-ao, more than five kilometers away from the Poblacion, Sierra Bullones.',
+    imageUrl: 'assets/img/paradiseLatest.jpg',
+    imageWidth: 350,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+}
+
+function riceTerraces()
+{
+  Swal.fire({
+    title: 'Rice Terraces',
+    text: 'The rice terraces of Sierra Bullones, Bohol are not listed with the wonders of the world but, however, they are very amazing. The hard work of the farmers has carved this landscape.',
+    imageUrl: 'assets/img/bohol rice terraces.jpg',
+    imageWidth: 350,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+}
+
+function riceTerraces2()
+{
+  Swal.fire({
+    title: 'Rice Terraces',
+    text: 'The rice terraces of Sierra Bullones, Bohol are not listed with the wonders of the world but, however, they are very amazing. The hard work of the farmers has carved this landscape.',
+    imageUrl: 'assets/img/rice_terraces.jpg',
+    imageWidth: 350,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+}
---
+++ website-baralgroup/assets/js/type.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.animate', {
+    strings: [
+        "Discover",
+        "Explore",
+        "Feel"
+    ],
+        typeSpeed: 80,
+        backSpeed: 80,
+        loop: true
+  });
